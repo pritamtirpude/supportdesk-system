e39237db503dd2162321f0274d1ff05dae8e704c
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2022-02-08 17:30:29 +0530

    created backend, added controllers, model, configs, routes

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+
+const { errorHandler } = require("../backend/middleware/errorMiddleware");
+
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 8000;
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
