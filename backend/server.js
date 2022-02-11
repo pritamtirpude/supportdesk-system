7e34728a66e03e353ee0892c50a6e045b64a2bb7
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2022-02-11 17:13:11 +0530

    Deployment process

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
@@ -23,6 +24,20 @@ app.get("/", (req, res) => {
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  // set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the Support Desk API" });
+  });
+}
 
 app.use(errorHandler);
 
